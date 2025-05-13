bd3f2a212f4f02b319b8ea32932f5f1f9dd760b7
Milena Narvaez <narvaezmilena084@gmail.com>
2025-05-13 15:45:54 -0500

    added terms

---
+++ terms.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+class PhotographicStyle(Enum):
+    Arquitectónico = "Arquitectónico"
+    Artístico = "Artístico"
+    Callejero = "Callejero"
+    Documental = "Documental"
+    Directa = "Directa"
+    Desnudos = "Desnudos"
+    Publicitaria = "Publicitaria"
+    Paisaje = "Paisaje"
+    Retrato = "Retrato"
+
+
+class Genre(Enum):
+    Femenino = "Femenino"
+    Masculino = "Masculino"
+
+
+class Nationality(Enum):
+    Argentina = "Argentina"
+    Australia = "Australia"
+    Alemania = "Alemania"
+    Brasil = "Brasil"
+    Colombia = "Colombia"
+    Canada = "Canadá"
+    Chile = "Chile"
+    China = "China"
+    Cuba = "Cuba"
+    Espania = "España"
+    Estados_unidos ="Estados Unidos"
+    Francia = "Francia"
+    Hungria = "Hungría"
+    Italia = "Italia"
+    Japon = "Japón"
+    Mexico = "México"
+    Reino_unido = "Reino Unido"
+
+
+
+
+
+
